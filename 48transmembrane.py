4d7f67f3f304f568b63846fcad710ab195416079
jafox85 <ztjiang@ucdavis.edu>
2025-03-16 12:07:48 -0700

    48transmembrane

--- 48transmembrane.py
+++ 48transmembrane.py
@@ -11,24 +11,17 @@ def kd(a):
 		if nt in code:
 			tot += score[code.index(nt)]
 	return tot / len(a)
-x = 0
+
+def if_transmembrane_protein(pep,w,t):
+	for i in range(len(pep) -w + 1):
+		seq = pep[i:i+w]
+		if kd(seq) >= t: return True
+	return False
+
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	signal_seq = seq[0:30]
-	trans_seq = seq[30:]
-	s = False
-	t = False
-	for i in range(len(signal_seq)-8+1):
-		b = signal_seq[i:i+8]
-		if kd(b) >= 2.5:
-			s = True
-			break
-	for i in range(len(trans_seq)-11+1):
-		c = trans_seq[i:i+11]
-		if kd(c) > 2:
-			t = True
-			break
-	if t == True and s == True:
+	n = seq[0:30]
+	c = seq[30:]
+	if if_transmembrane_protein(n, 8, 2.5) and if_transmembrane_protein(c, 11, 2):
 		print(defline)
-		x += 1
-print(x)
-			
+
+
